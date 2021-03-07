532147b42dc03e118a67aad7b367655d96ccbc0f
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-03-07 14:56:09 +0600

    07.03.2021

--- trait.php
+++ trait.php
@@ -1,10 +1,9 @@
 <?php
- class test{
-     public $_name;
+ trait test{
+     private $_name;
      public function setName($name){
          return $this->_name = $name;
      }
-
  }
 
  trait test2{
@@ -14,12 +13,13 @@
          return $this->_age = $y;
      }
      public function cop(){
-         return 'i am coping'.$this->_age;
+         return 'i am coping '.$this->_age;
      }
  }
 
- class test1 extends test{
-     use test2;
+ class test1{
+      use test2;
+      use test;
      public function getName(){
         return $this->_name;
      }
