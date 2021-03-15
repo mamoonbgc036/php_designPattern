35c2bee53d80f482e576af5465522aab8476b2a3
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-03-15 17:40:58 +0600

    15.03.2021

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+class test{
+    private $_name;
+    private $_age;
+
+    public function __construct($name, $age){
+        $this->_name = $name;
+        $this->_age = $age;
+    }
+
+    public static function show($name, $age){
+        $x = new test($name, $age);
+        return $x;
+    }
+}
+
+print_r(test::show('mamoon', 24));
