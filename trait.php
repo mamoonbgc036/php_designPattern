979241b85fb5954b5e42709ec3a6dbf36ae66514
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-05-30 15:11:08 +0600

    30.05.2021

--- trait.php
+++ trait.php
@@ -1,34 +1,71 @@
 <?php
- trait test{
-     private $_name;
-     public function setName($name){
-         $this->_name = $name;
-         return $this;
-     }
- }
+//  trait test{
+//      private $_name;
+//      public function setName($name){
+//          $this->_name = $name;
+//          return $this;
+//      }
+//  }
 
- trait test2{
-     private $_age;
+//  trait test2{
+//      private $_age;
 
-     public function setAge($y){
-         return $this->_age = $y;
-     }
-     public function cop(){
-         return 'i am coping '.$this->_age;
-     }
- }
+//      public function setAge($y){
+//          return $this->_age = $y;
+//      }
+//      public function cop(){
+//          return 'i am coping '.$this->_age;
+//      }
+//  }
 
- class test1{
-      use test2;
-      use test;
-     public function getName(){
-        return $this->_name;
-     }
- }
+//  class test1{
+//       use test2;
+//       use test;
+//      public function getName(){
+//         return $this->_name;
+//      }
+//  }
 
- $x = new test1();
- echo $x->setName('mamoon')->getName();
+//  $x = new test1();
+//  echo $x->setName('mamoon')->getName();
 // echo $x->getName();
 //  $x->setAge(25);
 //  echo $x->cop();
-?>
+
+
+trait test{
+    private $name;
+
+    public function setName($name){
+        $this->name = $name;
+        return $this;
+    }
+}
+
+trait test1{
+    private $age;
+
+    public function setAge($age){
+        $this->age = $age;
+        return $this;
+    }
+}
+
+class mamoon{
+    use test;
+    use test1;
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getAge(){
+        return $this->age;
+    }
+}
+
+$x = new mamoon();
+
+$nam = $x->setName('noman')->getName();
+
+echo $nam;
