50bda1e761897c5c58fc17bb13fb2e2e1a2c839c
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-06-06 12:20:14 +0600

    06.06.2021

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-
-function makeTest($a, $b){
-    $arr = array($a,$b,$a+$b);
-    if(in_array(10, $arr)){
-        return 'Found';
-    }else{
-        return 'Not Found';
-    }
-}
-
-echo makeTest(10,5);
+$a = "better";
+echo "something $a";
 
