e5fed65f53eaacab21e2bbfa43c412be38786271
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-06-06 16:11:07 +0600

    06.06.2021

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
  
- $x = str_split(23);
- var_dump($x);
+ function largestSwap($num) {
+    $x = str_split($num);
+    $y = $x[0];
+    $x[0] = $x[1];
+    $x[1] = $y;
+    $newOne = $x[0].$x[1];
+    if ($num<$newOne) {
+        echo true;
+    }else{
+        echo false;
+    }
+}
+
+largestSwap(43);
