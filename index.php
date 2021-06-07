de2a7beef816fe47e79acef48eccbe4a30da8128
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-06-07 14:25:55 +0600

    07.06.2021

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
- function averageCount($x){
-    $sum = 0;
-    $len = count($x);
-    for ($i=0; $i < $len ; $i++) { 
-        $sum+=$x[$i];
+ class Test{
+    public $aver;
+
+    public function averageCount($x){
+        $sum = 0;
+        $len = count($x);
+        for ($i=0; $i < $len ; $i++) { 
+            $sum+=$x[$i];
+        }
+        return $this->aver = $sum/$len;
     }
-    return $sum/$len;
  }
 
- echo averageCount([1,2,3,4]);
+ class TestOne{
+    public $obj;
+
+    public function __construct($arr){
+        $this->obj = new Test();
+        echo $this->obj->averageCount($arr);
+    }
+ }
+
+ new TestOne([1,2,3,4,5]);
