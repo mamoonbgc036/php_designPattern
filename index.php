f54e0b8fcf31b033986e64210801933866c7e581
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-06-08 12:45:46 +0600

    08.06.2021

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?php
  
- class Test{
-    public $name,$age;
-
-    public function __construct($name,$age){
-        $this->name = $name;
-        $this->age = $age;
-    }
- }
-
- class TestOne extends Test{
-    public function getName(){
-        return $this->name;
-    }
- }
-
- $x = new TestOne('noman',24);
- echo $x->getName();
+echo phpinfo();
--- namespacex/index.php
+++ namespacex/index.php
@@ -1,14 +1,3 @@
 <?php
-	use test\practics;
-	include_once('test/practics.php');
 
-	use test\practics1;
-	include_once('test1/practics1.php');
-
-	$x = new practics();
-
-	$x->show();
-
-	$x1 = new practics1();
-
-	$x1->show1();
+echo "tested";
