4097213a0a18d7440e875664efce04e9a4222b68
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-06-12 16:47:45 +0600

    12.06.2021

--- magic.php
+++ magic.php
@@ -14,4 +14,29 @@
 
  $x = new Test();
  $x->mamoon([12,24]);
- Test::noman('testing');
+ Test::noman(12,24,45);
+
+class kids
+{
+
+	public function __set($property, $value)
+	{
+		if ($value > 30)
+		{
+			$this->property= $value;
+		}
+	}
+
+	public function __get($property)
+	{
+		return "The child's height is " . $this->property . " inches tall";
+	}
+
+}
+
+$kid1= new kids;
+
+$kid1->height= 45;
+
+echo $kid1->height;
+var_dump($kid1);
