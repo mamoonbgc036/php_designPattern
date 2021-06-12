c7f91a6dd4548e414c108f2f226d9a12a5168c80
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-06-12 15:11:22 +0600

    12.06.2021

--- magic.php
+++ magic.php
@@ -5,7 +5,13 @@
  		echo $name;
  		print_r($argument);
  	}
+
+ 	public static function __callStatic($name, $argument){
+ 		echo $name;
+ 		echo implode(';', $argument);
+ 	}
  }
 
  $x = new Test();
- $x->mamoon('testing');
+ $x->mamoon([12,24]);
+ Test::noman('testing');
