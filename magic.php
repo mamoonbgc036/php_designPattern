cd7b9b811848f9d337cf0e1e993d373ca0c169af
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-06-13 16:39:26 +0600

    13.06.2021

--- magic.php
+++ magic.php
@@ -30,3 +30,32 @@ $x = new setGet();
 
 $x->name = "noman";
 $x->name;
+
+class Person
+{
+    public $sex;
+    private $name;
+    private $age;
+
+    public function __construct($name="",  $age=25, $sex='Male')
+    {
+        $this->name = $name;
+        $this->age  = $age;
+        $this->sex  = $sex;
+    }
+
+    /**
+     * @param $content
+     *
+     * @return bool
+     */
+    public function __isset($content) {
+        echo "The {$content} property is private，the __isset() method is called automatically.<br>";
+        echo  isset($this->$content);
+    }
+}
+
+$person = new Person("John", 25); // Initially assigned.
+echo isset($person->sex),"<br>";
+echo isset($person->name),"<br>";
+echo isset($person->age),"<br>";
