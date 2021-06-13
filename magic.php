a1977d8e0890c534cc911a602c7867e6f6544203
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-06-13 16:44:44 +0600

    13.06.2021

--- magic.php
+++ magic.php
@@ -1,35 +1,35 @@
 <?php
 
- class Test{
- 	public function __call($name, $argument){
- 		echo $name;
- 		print_r($argument);
- 	}
+ // class Test{
+ // 	public function __call($name, $argument){
+ // 		echo $name;
+ // 		print_r($argument);
+ // 	}
 
- 	public static function __callStatic($name, $argument){
- 		echo $name;
- 		echo implode(';', $argument);
- 	}
- }
+ // 	public static function __callStatic($name, $argument){
+ // 		echo $name;
+ // 		echo implode(';', $argument);
+ // 	}
+ // }
 
  // $x = new Test();
  // $x->mamoon([12,24]);
  // Test::noman(12,24,45);
 
-class setGet{
-	public function __set($property,$value){
-		return $this->property = $value;
-	}
+// class setGet{
+// 	public function __set($property,$value){
+// 		return $this->property = $value;
+// 	}
 
-	public function __get($property){
-		echo $this->property;
-	}
-}
+// 	public function __get($property){
+// 		echo $this->property;
+// 	}
+// }
 
-$x = new setGet();
+// $x = new setGet();
 
-$x->name = "noman";
-$x->name;
+// $x->name = "noman";
+// $x->name;
 
 class Person
 {
