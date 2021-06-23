ffc52a7e918cfdd4b1c424f1ba78717360cbd828
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-06-23 11:45:11 +0600

    23.06.2021

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php
     
-    function Textlenght($item){
-        return strlen($item);
-    }
-
     $string = ['mamoon','noman','sharif'];
 
     $len = array_map("Textlenght", $string);
 
     var_dump($len);
+
+    function Textlenght($item){
+        return strlen($item);
+    }
