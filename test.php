770be05c755d10538920b59fbcddfb122c018cf1
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-06-23 11:39:27 +0600

    23.06.2021

--- test.php
+++ test.php
@@ -1,24 +1,11 @@
 <?php
-    interface test{
-        public function get($name,$age);
-        public function show();
+    
+    function Textlenght($item){
+        return strlen($item);
     }
 
-    class dummy implements test{
-        private $_name,$_age;
+    $string = ['mamoon','noman','sharif'];
 
-        public function get($name,$age){
-            $this->_name = $name;
-            $this->_age = $age;
-        }
+    $len = array_map("Textlenght", $string);
 
-        public function show(){
-            return [$this->_name,$this->_age];
-        }
-    }
-
-    $x = new dummy();
-
-    $x->get("mamoon",32);
-
-    var_dump($x->show());
+    var_dump($len);
