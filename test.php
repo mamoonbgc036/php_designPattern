ac48453b44e171866d312acec83f8f73ec1cb6dc
Abdullah Al Mamoon <mamoonbgc0607@gmail.com>
2021-06-25 21:20:50 +0600

    25.06.2021

--- test.php
+++ test.php
@@ -7,3 +7,25 @@
             echo "test";
         }
     }
+
+
+    class Singleton{
+        private static $_instance;
+        private $_name;
+
+        public static function getInstance(){
+            if(!self::$_instance){
+                self::$_instance = new Singleton();
+            }
+
+            return self::$_instance;
+        }
+
+        public function getName($name){
+            $this->_name = $name;
+        }
+
+        public function showName(){
+            return $this->_name;
+        }
+    }
