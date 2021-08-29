98a7e2fada5479a714021af1734db37ad529219c
mamoonbgc036 <68588933+mamoonbgc036@users.noreply.github.com>
2021-08-29 11:16:49 +0600

    Update test.php

--- test.php
+++ test.php
@@ -121,3 +121,11 @@ class sendSMS implements Mamoon{
 	</script>
 </body>
 </html>
+function arraysSum(array ...$arrays)
+{
+    return array_map(function(array $array) {
+        return array_sum($array);
+    }, $arrays);
+}
+
+print_r(arraysSum([1,2,3], [4,5,6], [7,8,9]));
