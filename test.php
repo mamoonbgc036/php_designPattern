01d5c9c38baac23fa38b6c6dfae14bf7186b69d9
mamoonbgc036 <68588933+mamoonbgc036@users.noreply.github.com>
2021-09-02 12:13:32 +0600

    Update test.php

--- test.php
+++ test.php
@@ -159,3 +159,13 @@ class Motivation{
 		}
 	}
 }
+
+<head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+</head>
+<body>
+<h1 class="animate__animated animate__bounce">An animated element</h1>	
+</body>
