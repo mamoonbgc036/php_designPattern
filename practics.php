a7db1557d6ba1ac3515e3036701b17714aea66b4
mamoonbgc036 <68588933+mamoonbgc036@users.noreply.github.com>
2021-11-16 12:53:28 +0600

    Update practics.php

--- practics.php
+++ practics.php
@@ -72,3 +72,61 @@
 echo date('h:i:s').'<br>';
   sleep(5);
   echo date('h:i:s');
+
+//convert string to number
+echo convertWord('zero,one,two,three,four,five,six,seven,eight,nine');
+
+function convertWord($str){
+  $result = '';
+  $words = explode(',', $str);
+  foreach ($words as $word) {
+    switch ($word) {
+      case 'zero':
+        $result .= '0';
+        break;
+
+      case 'one':
+        $result .= '1';
+        break;
+
+      case 'two':
+        $result .= '2';
+        break;
+
+      case 'three':
+        $result .= '3';
+        break;
+
+      case 'four':
+        $result .= '4';
+        break;
+      
+      case 'five':
+        $result .= '5';
+        break;
+
+      case 'six':
+        $result .= '6';
+        break;
+
+      case 'seven':
+        $result .= '7';
+        break;
+
+      case 'eight':
+        $result .= '8';
+        break;
+
+      case 'nine':
+        $result .= '9';
+        break;
+
+      default:
+        echo "default";
+        break;
+    }
+  }
+
+  return $result;
+}
+
