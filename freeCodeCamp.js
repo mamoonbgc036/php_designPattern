c19e05c5fb0addde8ea79b7e062e35b7c892e0f2
mamoonbgc036 <68588933+mamoonbgc036@users.noreply.github.com>
2021-12-19 12:44:11 +0600

    Update and rename practic.js to freeCodeCamp.js

---
+++ freeCodeCamp.js
@@ -0,0 +1,51 @@
+//trenary operator
+function check(a, b){
+  return (a===b) ? 'a and b is equal' : (a < b) ? 'a is smaller' : 'b is smaller';
+}
+
+console.log( check(10, 8) )
+
+//convert string to number
+parseInt('12');
+//generate random number
+function randomRange(myMin, myMax) {
+  const test = Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  return test;
+}
+
+console.log( randomRange(10, 1000) );
+
+//JAVASCRIPT LOOKUP
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: [ 'Intriguing Cases', 'Violin' ],
+  },
+];
+
+function lookUpProfile( name, prop ){
+  const x = contacts.filter(con => con.firstName === name );
+  return x.length ? x[0][prop] ? x[0][prop] : 'No such property' : 'No such contact';
+}
+console.log(lookUpProfile("Sherlock", "likes"));
