8b83e1f49c0432c96282d38583278e2c466c2c4d
mamoonbgc036 <68588933+mamoonbgc036@users.noreply.github.com>
2021-12-19 11:08:30 +0600

    Update practic.js

--- practic.js
+++ practic.js
@@ -7,3 +7,10 @@ console.log( check(10, 8) )
 
 //convert string to number
 parseInt('12');
+//generate random number
+function randomRange(myMin, myMax) {
+  const test = Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  return test;
+}
+
+console.log( randomRange(10, 1000) );
