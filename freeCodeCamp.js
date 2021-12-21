8f1b90be906e964af6ab586cf11efcfe82560011
mamoonbgc036 <68588933+mamoonbgc036@users.noreply.github.com>
2021-12-21 16:41:00 +0600

    Update freeCodeCamp.js

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -72,3 +72,21 @@ function sum(arr, n) {
 }
 
 console.log(sum([1,2,3], 3));
+
+//nested for loop
+
+const arr = [
+  [1,2], [3,4], [5,6]
+];
+
+function multiply(arr){
+  let product = 1;
+  for (let i = 0; i < arr.length; i++){
+  for (let j = 0; j < arr[i].length; j++){
+    product *= arr[i][j];
+  }
+}
+return product;
+}
+
+console.log(multiply(arr));
