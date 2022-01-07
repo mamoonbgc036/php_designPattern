77a3b83c1b5a8cd38ed36e69f57171d9dcb9dd0f
mamoonbgc036 <68588933+mamoonbgc036@users.noreply.github.com>
2022-01-07 15:28:33 +0600

    Update freeCodeCamp.js

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -271,6 +271,22 @@ function reverseWord(word){
 
 console.log(reverseWord('mamoon'))
 
+function reverseWord(word){
+  let newWord = '';
+  for(let i = word.length-1;i >= 0; i--){
+    newWord += word[i];
+  }
+  if(newWord==word){
+    return 'this is palindrom';
+  }else{
+    return 'this is not palindrom';
+  }
+}
+
+console.log(reverseWord('aba'))
+
+
+
 
 
 
