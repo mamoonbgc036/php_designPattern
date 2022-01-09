c5dac9b1959963d79eea4583162c403d9dc58414
mamoonbgc036 <68588933+mamoonbgc036@users.noreply.github.com>
2022-01-09 11:04:44 +0600

    Update freeCodeCamp.js

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -285,6 +285,23 @@ function reverseWord(word){
 
 console.log(reverseWord('aba'))
 
+function sumAll(arr) {
+  let sum = 0;
+  if(arr[0]<arr[1]){
+    for(let i = arr[0]; i <= arr[1]; i++){
+      sum += i;
+    }
+  }else{
+    for(let i = arr[1]; i <= arr[0]; i++){
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+console.log(sumAll([1, 4]));
+
 
 
 
